eff27270d58317c3a043dd056229736cb8d2cd50
nachom <nachomdev@gmail.com>
2025-11-26 19:54:32 -0300

    varios

--- pastapp/urls.py
+++ pastapp/urls.py
@@ -6,6 +6,6 @@ from . import views
 urlpatterns = [
     path('', views.index, name='index'),
     path('past', views.past, name='pasto'),
-    path('pastold', views.index_past, name='past'),
-    path('pastest', views.pastest, name='pastest'),
+    path('juan', views.juan, name='pastojuan'),
+    path('azar', views.azar, name='pastoazar'),
 ]
--- pastapp/views.py
+++ pastapp/views.py
@@ -6,9 +6,14 @@ def index(request):
 def past(request):
     return render(request, 'pastapp/pasto.html', {})
 
-
 def index_past(request):
     return render(request, 'pastapp/past.html', {})
 
 def pastest(request):
     return render(request, 'pastapp/pastest.html', {})
+    
+def juan(request):
+    return render(request, 'pastapp/pastojuan.html', {})
+    
+def azar(request):
+    return render(request, 'pastapp/pastoazar.html', {})
